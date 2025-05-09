a1bf81c6ed2fda6f112211311adb3293b2c1e1a6
mozlercelik <celikmehmetozler@gmail.com>
2025-05-09 13:21:54 +0300

    Project name update

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api', (req, res) => {
-    res.send('Welcome to the Attack Surface Management Tool!');
+    res.send('Welcome to Easycan!');
 });
 
 app.get("/api/test", (req, res) => {
